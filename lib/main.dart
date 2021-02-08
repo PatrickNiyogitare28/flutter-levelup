6b48ba6c04f2e1395c4c597da7aeeb039c383959
Patrick <patrickniyogitare28@gmail.com>
2021-02-08 17:36:15 +0200

    fit: added login #4.58

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,9 @@ class MyApp extends StatelessWidget {
    @override
    Widget build(BuildContext context){
      return MaterialApp(
-       title: 'Welcome to my Flutter Level up app',
-       home: Scaffold(
-         appBar: AppBar(
-           title: Text('Welcome to my Flutter level up app'),
-         ),
-         body: Center(
-           child: Text("Hello world"),
-         ),
-       ),
+       debugShowCheckedModeBanner: true,
+       home: LoginPage();
      );
    }
 }
+
